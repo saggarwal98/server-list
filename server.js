b3971bf274fc9ed0363d324c6b1c7cd4548202a0
saggarwal98 <shubhamaggarwalmvn@gmail.com>
2018-07-29 22:30:35 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app=express()
+
+const todos = ["one task", "two task"]
+
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => res.send(`
+<form method="post" action="/addtodo">
+  <input name="newtodo">
+  <type="submit">
+</form>
+ <ul>
+  <li>
+  ${todos.join('</li><li>')}
+  </li>
+ </ul>
+`))
+
+app.get('/addtodo', (req, res) => {
+  todos.push(req.query['newtodo'])
+  res.redirect('/') 
+})
+
+app.post('/addtodo',(req,res)=>{
+    todos.push(req.body['newtodo'])
+    res.redirect('/')
+})
+const port=process.env.port||3333
+app.listen(port, () => console.log(`
+Server started on http://localhost:${port}
+`))
